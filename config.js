d6f58b127da9402428d5bdcb561b54cf51b2ee60
Ed Kelly <ed@newlinelabs.com>
2019-05-05 22:22:49 +0100

    Add classifier

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 module.exports = {
-  url: 'https://www.reddit.com/r/all/comments/.json?limit=100'
+  url: 'https://www.reddit.com/r/all/comments/.json?limit=100',
+  lang: 'en'
 }
--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const CommentStream = require('./lib/comment-stream')
+const Tokeniser = require('./lib/tokeniser')
+const Classifier = require('./lib/classifier')
 const config = require('./config')
 
 const commentStream = new CommentStream(config.url)
+const tokeniser = new Tokeniser()
+const classifier = new Classifier(config.lang)
 
-commentStream.on('comment', comment => console.log(`${comment.subreddit}: ${comment.body}`))
+commentStream.on('comment', (comment) => {
+  const tokens = tokeniser.tokenise(comment.body)
+  const score = classifier.score(tokens)
+
+  score.subreddit = comment.subreddit
+  score.comment = comment.body
+})
+
 commentStream.on('error', err => console.error(err))
 
 commentStream.start()
